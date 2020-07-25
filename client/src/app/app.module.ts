afa9fdb49bb1e5b43c30b0bf7472a4aee8294462
vattcarter7 <vattcarter@gmail.com>
2020-07-25 20:38:57 +0700

    style shop page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
